ba4264fa83f896c26af810725b12848795d1ad0a
sajanjacob <sajanjacob67@gmail.com>
2022-03-20 18:17:41 -0600

    Updated phone number

--- utils/getLocation.js
+++ utils/getLocation.js
@@ -1,24 +1,14 @@
 export default function getLocation(location) {
 	const locations = {
 		edmonton: {
-			title: "Prana Party",
+			title: "Eco Apps",
 			address: "7450 82 Ave NW",
 			city: "Edmonton",
 			province: "AB",
-			phone: "+1-780-246-3275",
-		},
-		whitecourt: {
-			title: "Soban Korean Japanese Cuisine",
-			address: "5108 50 Ave #4",
-			city: "Whitecourt",
-			province: "AB",
-			phone: "+1-780-778-5225",
+			phone: "+(587) 804-0608",
 		},
 	};
 	if (location === "edmonton" || location === "Edmonton") {
 		return locations.edmonton;
-	}
-	if (location === "whitecourt" || location === "Whitecourt") {
-		return locations.whitecourt;
 	}
 }
